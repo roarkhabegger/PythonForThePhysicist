596f6bb4c9fa81e8757aeea3b95558a427dbff17
Roark <roarkhabegger@gmail.com>
2020-06-11 20:28:47 -0400

    Update 6_11_2020

---
+++ Intro_To_Python/lesson01.py
@@ -0,0 +1,28 @@
+#Hello Students! I hope this python file finds you healthy and ready to learn!
+#Section 1: Variables & Data types
+var1 = 3.14
+
+var1 = 'Hi my name is Roark'
+
+
+#Section 2: Printing
+#Python3 treats print as a function. Hopefully this is the python version you
+#have installed
+#A function takes in input and then provides output.
+print("Hello World")
+
+#Python 2 does not treat print as a function.
+#In python2, 'print "Hello World"' is the correct syntax
+
+#For print, the parameter you pass into the function
+#is the string or number or array that you want printed
+p = print("Hello World")
+print(p)
+#In detail, print actually has no output! It returns None which is not any kind of variable.
+#None is not even a string!
+
+#Let's look at variables. Python is unlike C-based languages,
+#becuase you do not have to declare the data type of the string.
+
+i = 10
+x = -5.0
