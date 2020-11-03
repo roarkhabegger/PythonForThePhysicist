42b525a39587e5f790e2f7df5522e2f6ef74be19
Roark <roarkhabegger@gmail.com>
2020-11-02 19:14:38 -0600

    New Lesson 2

--- Intro_To_Python/Lesson02_Boolean/lesson02.py
+++ Intro_To_Python/Lesson02_Boolean/lesson02.py
@@ -2,12 +2,22 @@
 #Lesson02.py
 #Hello Students! I hope this python file finds ready to learn!
 
+#What does Boolean Mean
+myVar = True
+
+#Logic 101
+a = 1.0
+b = 2.0
+aEqualb = (a==b)
+aNEqualb = a!=b
+print(aEqualb)
+print(aNEqualb)
 
 ##################
 ### PROBLEM 01 ###
-
+#compare a,b with >, <, >=, <=
 
 ##################
 ##################
 
-#Addition, Division, and all that Jazz
+#If else elif and or
